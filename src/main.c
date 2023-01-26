9c556a6de2cb28a83a11861673932c0e1b99c8c3
Logan Finley <finley4.logan@gmail.com>
2023-01-26 10:24:37 -0800

    Updated project structure with an include dir and updated cmake to check for FreeRTOS-Kernel in PICO_SDK_PATH/../

--- src/main.c
+++ src/main.c
@@ -1,7 +1,34 @@
+#include <FreeRTOS.h>
+#include <task.h>
+#include <stdio.h>
 #include "pico/stdlib.h"
 
-#define LED_PIN PICO_DEFAULT_LED_PIN
+void task_1();
+void task_2();
 
 int main()
 {
+    stdio_init_all();
+
+    xTaskCreate(task_1, "Task_1", 256, NULL, 1, NULL);
+    xTaskCreate(task_2, "Task_2", 256, NULL, 1, NULL);
+    vTaskStartScheduler();
+
+    while(1){};
+}
+
+void task_1()
+{
+    while (true) {
+        printf("Hello from task 1\n");
+        vTaskDelay(200);
+    }
+}
+
+void task_2()
+{
+    while (true) {
+        printf("Hello from task 2\n");
+        vTaskDelay(200);
+    }
 }
