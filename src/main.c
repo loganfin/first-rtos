3472eb5481e03d43210d184bd0390243a14cd419
Logan Finley <finley4.logan@gmail.com>
2023-01-26 20:28:21 -0800

    Print to the serial console every second

--- src/main.c
+++ src/main.c
@@ -11,7 +11,6 @@ int main()
     stdio_init_all();
 
     xTaskCreate(task_1, "Task_1", 256, NULL, 1, NULL);
-    xTaskCreate(task_2, "Task_2", 256, NULL, 1, NULL);
     vTaskStartScheduler();
 
     while(1){};
@@ -19,16 +18,12 @@ int main()
 
 void task_1()
 {
-    while (true) {
-        printf("Hello from task 1\n");
-        vTaskDelay(200);
-    }
-}
+    uint64_t current_time = 0;
 
-void task_2()
-{
+    const TickType_t xDelay1000ms = pdMS_TO_TICKS(1000);
     while (true) {
-        printf("Hello from task 2\n");
-        vTaskDelay(200);
+        current_time = get_absolute_time() / 1000000;
+        printf("seconds since boot: %lld \n", current_time);
+        vTaskDelay(xDelay1000ms);
     }
 }
